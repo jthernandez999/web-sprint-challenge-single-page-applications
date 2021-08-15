040620c20e650c35662db2ff4bc6875746a4ce81
Joe Hernandez <j.t.hernandez999@gmail.com>
2021-08-15 13:59:20 -0700

    added more styling

--- src/Home.js
+++ src/Home.js
@@ -13,11 +13,15 @@ console.log('history', history)
     return (
 <div className='home-wrapper'>
     <h1>Gourmet Pizza</h1>
+
     <img
     className='home-image'
     src='https://www.eatthis.com/wp-content/uploads/sites/4/2020/07/spanish-style-pizza.jpg?quality=82&strip=1&resize=1250%2C702'
-    alt=''
-    />
+    alt=''/>
+    <h2 className='first-text'>Your favorite food, delivered while coding</h2>
+    
+
+
 
     <Link to='/pizza'>
     <button className='md-button' id="order-pizza" >Pizza?</button>
